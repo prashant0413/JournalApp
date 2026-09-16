82ff5abce6512727a30591126c20b4645a868367
prashant0413 <prashantvishwakarma0413@gmail.com>
2026-09-16 15:03:06 +0530

    sentiment analysis logic

---
+++ src/test/java/net/engineeringdigest/journalApp/scheduler/UserSchedulerTests.java
@@ -0,0 +1,16 @@
+package net.engineeringdigest.journalApp.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserSchedulerTests {
+    @Autowired
+    private UserScheduler userScheduler;
+
+    @Test
+    public void testFetchUserAndSendSaMail() {
+        userScheduler.fetchUserAndSendSaMail();
+    }
+}
