d9ba61bed2e82c1c3352a487db266722d7a5c411
prashant0413 <prashantvishwakarma0413@gmail.com>
2026-09-17 20:49:13 +0530

    redis integeration

---
+++ src/test/java/net/engineeringdigest/journalApp/service/RedisTests.java
@@ -0,0 +1,18 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testSendMail() {
+        redisTemplate.opsForValue().set("email", "gmail@email.com");
+        Object email = redisTemplate.opsForValue().get("email");
+    }
+}
